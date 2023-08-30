b18fbd7b6383c3a84de1aa27a8ed8f1af9f1354a
Shakil53 <shakilahmed.dcc@gmail.com>
2023-08-30 23:14:17 +0600

    nav & navPlus & carosule added by shakil

---
+++ src/components/NavPlus/NavPlus.jsx
@@ -0,0 +1,13 @@
+import pic from '../../assets/img/drmmrpc-2.png'
+const NavPlus = () => {
+    return (
+        <div className="max-w-6xl ml-48 carousel rounded-box">
+            <div className="carousel-item w-full">
+                <img src={pic} className="w-full" alt="Tailwind CSS Carousel component" />
+            </div>
+
+        </div>
+    );
+};
+
+export default NavPlus;
